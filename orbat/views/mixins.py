32ae678aa5db5cd39a06bc490d3ef8ea6b26844b
John Mullins <john.mullins@education.vic.gov.au>
2026-02-28 20:43:55 +1100

    Added users section shortcut to the orbat sidebar

--- orbat/views/mixins.py
+++ orbat/views/mixins.py
@@ -1,3 +1,4 @@
+from orbat.helpers import get_section_for_user
 from orbat.permission_helpers import can_manage_orbat
 
 
@@ -19,6 +20,12 @@ class ORBATContextMixin:
             {"name": "Timeline", "path": "/orbat/timeline/"},
             {"name": "Applications", "path": "/orbat/applications/"},
         ]
+
+        section = get_section_for_user(user)
+        if section:
+            sidebar.append(
+                {"name": f"{section.name} Section Home", "path": "/orbat/section/" + section.slug + "/"}
+            )
 
         if context["show_management"]:
             sidebar.append(
